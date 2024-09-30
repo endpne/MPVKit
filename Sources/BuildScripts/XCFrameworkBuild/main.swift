18a2e8f32a1836547a128d5f044e291f0dbcfaf5
endpne <160698812+endpne@users.noreply.github.com>
2024-09-30 01:03:48 +0000

    chore: mpv bump version to v0.39.0%0A

--- Sources/BuildScripts/XCFrameworkBuild/main.swift
+++ Sources/BuildScripts/XCFrameworkBuild/main.swift
@@ -45,7 +45,7 @@ enum Library: String, CaseIterable {
     var version: String {
         switch self {
         case .libmpv:
-            return "v0.38.0"
+            return "v0.39.0"
         case .FFmpeg:
             return "n7.0.1"
         case .openssl:
